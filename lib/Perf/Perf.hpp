1ed736f8b0a43d4b524e7a2040b897494833fd7f
sciyen <a885566885566@gmail.com>
2024-01-22 11:56:34 +0800

    Using milliseconds instead of microseconds
    - Add additional check for millis overflow
    - Static variables generate bugs

--- lib/Perf/Perf.hpp
+++ lib/Perf/Perf.hpp
@@ -1,33 +1,45 @@
 #pragma once
 
 #include <Arduino.h>
+#include <semphr.h>
 
 class Perf {
 public:
-    Perf(int num_sampling=100, time_t timeout_ms=100) : 
+    typedef unsigned long utime_t;
+    Perf(int num_sampling=100, utime_t timeout_ms=200) : 
         _max_num_sampling(num_sampling), _timeout(timeout_ms), 
         _samples_idx(0), _fps(0)
     {
-        _last_update_time = micros();
+        _last_update_time = millis();
+        _last_recv_time = millis();
+        _mutex = xSemaphoreCreateMutex();
     }
 
-    void feed_data(uint32_t receive_time)
+    void feed_data(const utime_t receive_time)
     {
-        if (_samples_idx == _max_num_sampling - 1){
-            _fps = (double)_max_num_sampling / (receive_time - _last_update_time);
-            _last_update_time = receive_time;
+        const utime_t now = millis();
+        if (xSemaphoreTake(_mutex, 0) == pdTRUE) {
+            if (_samples_idx == _max_num_sampling - 1){
+                _fps = (float)_max_num_sampling / (now - _last_recv_time);
+                _last_recv_time = now;
+            }
+            _last_update_time = now;
+            _samples_idx = (_samples_idx + 1) % _max_num_sampling;
+            xSemaphoreGive(_mutex);
         }
-        _samples_idx = (_samples_idx + 1) % _max_num_sampling;
     };
 
     bool is_timeout() const {
-        return (micros() - _last_update_time > _timeout * 1e3);
+        const utime_t now = millis(); 
+        if (now < _last_update_time)
+            return (0xFFFFFFFF - _last_update_time + now);
+        return (now - _last_update_time > _timeout);
     };
 
-    double get_fps() const {
+    float get_fps() const {
         if ( is_timeout() )
             return -1;
-        return 1e6 * _fps;
+        return 1e3 * _fps;
     };
 
     ~Perf() {};
@@ -36,7 +48,9 @@ private:
     int _samples_idx;
     int _max_num_sampling;
     double _fps;
+    SemaphoreHandle_t _mutex{nullptr};
 
-    time_t _last_update_time;
-    time_t _timeout;            // in ms
+    utime_t _last_update_time;
+    utime_t _last_recv_time;
+    utime_t _timeout;            // in ms
 };
